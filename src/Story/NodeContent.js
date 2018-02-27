c9eb057c46e1e9d77fcbba7e3ed46e33245f158e
Steven Washington <swashington@connellypartners.com>
2018-02-27 14:57:14 -0500

    Making text scroll its own component
    Also allowing line breaks and spacing in text nodes...using white-space:pre

--- src/Story/NodeContent.js
+++ src/Story/NodeContent.js
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
+import TextScroll from '@@/TextScroll'
 
 import './NodeContent.scss';
 
-let printTimeout = null;;
-
 class NodeContent extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      content:""
-    }
-  }
-
-  componentDidMount() { if (this.props.content) this.printContent(this.props.content, true); }
-  componentDidUpdate(prevProps) {
-    if (prevProps.content !== this.props.content) { this.printContent(this.props.content, true) }
-  }
-
-  printContent(content, doReset) {
-    if (printTimeout) clearTimeout(printTimeout);
-
-    const nextVal = doReset ? "" : content.substring(0, this.state.content.length+1);
-    this.setState({ ...this.state, content:nextVal }, () => {
-      if (this.state.content.length < content.length) {
-        printTimeout = setTimeout(() => {
-          this.printContent(content)
-        }, 10);
-      }
-    })
-  }
 
   render() {
     return (
       <div className='node-content' style={{minHeight:"90px"}}>
-        {this.state.content}
+        <TextScroll>{this.props.content}</TextScroll>
       </div>
     )
   }
---
+++ src/TextScroll.js
@@ -0,0 +1,58 @@
+import React, { Component, Fragment } from 'react';
+
+
+let printTimeout = null;
+
+class TextScroll extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content:""
+    }
+  }
+
+  componentDidMount() {
+    if (typeof this.props.children !== 'string') {
+      throw new Error(`Only text children are allowed for TextScroll. Received ${this.props.children.map ? this.props.children.map(c => c.type).join(',') : this.props.children.type}`);
+    }
+
+    if (this.props.children) {
+      this.printContent(this.props.children, true);
+    } 
+  }
+
+  componentDidUpdate(prevProps) {
+    if (typeof this.props.children !== 'string') {
+      throw new Error(`Only text children are allowed for TextScroll. Received ${typeof this.props.children}`);
+    }
+
+    if (prevProps.children !== this.props.children) { 
+      this.printContent(this.props.children, true) 
+    }
+  }
+
+  printContent(content, doReset) {
+    if (printTimeout) clearTimeout(printTimeout);
+
+    const nextVal = doReset ? "" : content.substring(0, this.state.content.length+1);
+    this.setState({ ...this.state, content:nextVal }, () => {
+      if (this.state.content.length < content.length) {
+        printTimeout = setTimeout(() => {
+          this.printContent(content)
+        }, 10);
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.content}
+      </Fragment>
+    )
+  }
+}
+
+export default TextScroll;
